838b10e0cb4e001b9ad3ac71851121905cc69962
Nikita Maruniak <nikita.marunyak@gmail.com>
2020-01-08 21:09:45 +0100

    Run unit tests as a part of the pre-commit hook

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -6,37 +6,38 @@ import math
 sys.path.append("../")
 from ahocorasick import Dictionary
 
-if len(sys.argv) != 3:
-    sys.exit(1)
-else:
-    dict_path = sys.argv[1]
-    text_path = sys.argv[2]
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        sys.exit(1)
+    else:
+        dict_path = sys.argv[1]
+        text_path = sys.argv[2]
 
-    patterns = []
-    with codecs.open(dict_path, "r", "utf-8", errors="replace") as f:
-        for pattern in f:
-            pattern = pattern.strip()
-            patterns.append(pattern)
+        patterns = []
+        with codecs.open(dict_path, "r", "utf-8", errors="replace") as f:
+            for pattern in f:
+                pattern = pattern.strip()
+                patterns.append(pattern)
 
-    with codecs.open(text_path, "r", "utf-8", errors="replace") as f:
-        text = f.read()
+        with codecs.open(text_path, "r", "utf-8", errors="replace") as f:
+            text = f.read()
 
-    start = time.time()
-    d = Dictionary(patterns)
-    count = sum((1 for _ in d.matches(text)))
-    end = time.time()
-    ellapsed = (end - start) * 1000.0
-    text_size = len(text)
-    dict_size = sum((len(p) for p in patterns))
-    print(
-        "{},{},{},{},{},{},{},{}".format(
-            dict_path,
-            dict_size,
-            text_path,
-            text_size,
-            count,
-            ellapsed,
-            math.log(ellapsed, 2),
-            ellapsed / (text_size + dict_size),
+        start = time.time()
+        d = Dictionary(patterns)
+        count = sum((1 for _ in d.matches(text)))
+        end = time.time()
+        ellapsed = (end - start) * 1000.0
+        text_size = len(text)
+        dict_size = sum((len(p) for p in patterns))
+        print(
+            "{},{},{},{},{},{},{},{}".format(
+                dict_path,
+                dict_size,
+                text_path,
+                text_size,
+                count,
+                ellapsed,
+                math.log(ellapsed, 2),
+                ellapsed / (text_size + dict_size),
+            )
         )
-    )
--- pre-commit.sh
+++ pre-commit.sh
@@ -10,4 +10,14 @@ else
   echo "Code syle check passed."
 fi
 
+echo "Running unit tests..."
+python -m doctest *.py **/*.py
+if [ $? -ne 0 ]
+then
+  echo "Unit tests failed."
+  exit 1
+else
+  echo "Unit tests passed."
+fi
+
 echo "All checks passed."
